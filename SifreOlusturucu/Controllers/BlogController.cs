0e114129781cc3d7d6c9102a8e0c257dcc4d2dc0
Yavuz Yazici <4yavuzyazici@gmail.com>
2024-09-15 11:21:08 +0300

    Project Uploaded

---
+++ SifreOlusturucu/Controllers/BlogController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SifreOlusturucu.Controllers
+{
+    public class BlogController : Controller
+    {
+        public IActionResult EDevletSifreOlusturucu()
+        {
+            return View();
+        }
+        public IActionResult GuclutSifreNasilOlusturulur()
+        {
+            return View();
+        }
+    }
+}
